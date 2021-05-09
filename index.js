3926e2e13a17fe0efa82de33d37702209023aa9c
harshit saxena <harshitsaxena586@gmail.com>
2021-05-10 00:06:11 +0530

    updaeted index

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-require("dotenv").config();
 const express = require('express');
-const bodyParser = require('body-parser')
-const cors = require("cors")
-const port = process.env.PORT || 3000;
+const port = process.env.PORT
 const app = express();
-app.use(cors())
-
-const { initializeUserbase } = require('./IntialiseDB/UserbaseDB.js');
-initializeUserbase()
-
-const users = require("./Routes/users.js")
-app.use("/users", users)
+var router = express.Router()
 
 
-const products = require("./Routes/products.js")
-app.use("/products", products)
-
-const cart = require("./Routes/cart.js");
-app.use("/cart", cart)
 
 app.get('/', (req, res) => {
-  res.send('hello backend app!')
+  res.send('hello video backend app !')
 });
 
-app.listen(port, () => {
+app.listen(port || 3000, () => {
   console.log('server started');
 });
